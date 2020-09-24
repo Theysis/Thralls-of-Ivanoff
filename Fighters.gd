1ac7d0b4bea50a11bd4327c327e0572bb7983eca
Joshua <jwyattprofessional@gmail.com>
2020-09-24 14:27:49 -0500

    Combat Arena Test

---
+++ Fighters.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+class_name Fighters
+
+# Defines variables for character stats
+var st # Strength Stat
+var agil # Agility
+var def # Defence
+var intel # Intelligence
+var HP # Health
+var MP # Magic Points / Ability Points
+var fighter = [HP, MP, st, agil, def, intel]
+
+var runAway
+
+func intitialize() :
+	pass
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
---
+++ README.md
@@ -0,0 +1,1 @@
+Thralls-of-Ivanoff
